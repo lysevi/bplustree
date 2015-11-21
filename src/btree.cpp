5cdf795dd4d097aa48945c1d7d55e12df1ad8f20
Sergey Lyubimov <lysevi@gmail.com>
2015-11-21 11:27:16 +0300

    refact.

--- src/btree.cpp
+++ src/btree.cpp
@@ -8,34 +8,34 @@ BTree::Node::Node() {
 }
 
 BTree::Node::~Node() {
-	this->childs.clear();
-	this->vals.clear();
+    this->childs.clear();
+    this->vals.clear();
 }
 
 void BTree::Node::insertValue(int key) {
 
-	if (vals.size() == 0) {
-		vals.push_back(key);
-		return;
-	} else {
-		if (*vals.begin() > key) {
-			vals.insert(vals.begin(), key);
-			return;
-		}
-	}
+    if (vals.size() == 0) {
+        vals.push_back(key);
+        return;
+    } else {
+        if (*vals.begin() > key) {
+            vals.insert(vals.begin(), key);
+            return;
+        }
+    }
 
-	for (auto it = this->vals.begin(); it != this->vals.end(); ++it) {
-		if (it != this->vals.begin()) {
+    for (auto it = this->vals.begin(); it != this->vals.end(); ++it) {
+        if (it != this->vals.begin()) {
             auto cur=*it;
             auto prev=*(it-1);
             if ((key < cur) && (key > prev)) {
-				this->vals.insert(it, key);
-				return;
-			}
-		}
-	}
+                this->vals.insert(it, key);
+                return;
+            }
+        }
+    }
 
-	this->vals.push_back(key);
+    this->vals.push_back(key);
 }
 
 void BTree::Node::insertChild(int key, Node::Ptr C){
@@ -57,27 +57,27 @@ void BTree::Node::insertChild(int key, Node::Ptr C){
     assert(false);
 }
 BTree::BTree(size_t N) :n(N) {
-	m_root = BTree::make_node();
+    m_root = BTree::make_node();
 }
 
 BTree::~BTree() {
-	m_root = nullptr;
+    m_root = nullptr;
 }
 
 BTree::Node::Ptr BTree::make_node() {
-	return std::make_shared<BTree::Node>();
+    return std::make_shared<BTree::Node>();
 }
 
 int BTree::find(int key)const {
     Node::Ptr node = m_root;
-	int res;
-	if (this->iner_find(key,m_root, node, res)) {
-		assert(node != nullptr);
-		return res;
-	} else {
-		assert(node != nullptr);
-		return 0;
-	}
+    int res;
+    if (this->iner_find(key,m_root, node, res)) {
+        assert(node != nullptr);
+        return res;
+    } else {
+        assert(node != nullptr);
+        return 0;
+    }
 }
 
 bool BTree::iner_find(int key, Node::Ptr cur_node, Node::Ptr&out_ptr, int &out_res)const {
@@ -97,129 +97,123 @@ bool BTree::iner_find(int key, Node::Ptr cur_node, Node::Ptr&out_ptr, int &out_r
         }
     }
     for (size_t i = 0; i<cur_node->vals.size();i++){
-		if (cur_node->vals[i] == key) {
-			out_res = cur_node->vals[i];
-			out_ptr = cur_node;
-			return true;
-		}
-		if (cur_node->vals[i] > key) {
+        if (cur_node->vals[i] == key) {
+            out_res = cur_node->vals[i];
+            out_ptr = cur_node;
+            return true;
+        }
+        if (cur_node->vals[i] > key) {
             if ((cur_node->childs.size() > 0) && (i!=0)) {
                 return iner_find(key, cur_node->childs[i], out_ptr, out_res);
-            } /*else {
-
-                    out_res = 0;
-                    out_ptr = cur_node;
-                    return false;
-
-            }*/
-		}
-	}
-	if ((cur_node->childs.size() != 0) && (cur_node->vals.size() != 0)) {
-		if (key< cur_node->vals.front()) {
-			return iner_find(key, cur_node->childs.front(), out_ptr, out_res);
-		}
-		if (key> cur_node->vals.back()) {
-			return iner_find(key, cur_node->childs.back(), out_ptr, out_res);
-		}
-	} else {
-		out_res = 0;
-		out_ptr = cur_node;
-		return false;
-	}
+            }
+        }
+    }
+    if ((cur_node->childs.size() != 0) && (cur_node->vals.size() != 0)) {
+        if (key< cur_node->vals.front()) {
+            return iner_find(key, cur_node->childs.front(), out_ptr, out_res);
+        }
+        if (key> cur_node->vals.back()) {
+            return iner_find(key, cur_node->childs.back(), out_ptr, out_res);
+        }
+    } else {
+        out_res = 0;
+        out_ptr = cur_node;
+        return false;
+    }
     return false;
 }
 
 bool BTree::isFull(const BTree::Node::Ptr node)const {
-	return node->vals.size() >= (2 * n);
+    return node->vals.size() >= (2 * n);
 }
 
 bool BTree::insert(int key) {
     Node::Ptr node = nullptr;
-	int res;
-	if (this->iner_find(key,m_root, node, res)) {
-		return false;
-	} else {
-		assert(node != nullptr);
-		if (!isFull(node)) {
-			node->insertValue(key);
-			return true;
-		} else { //split
-			node->insertValue(key);
-			this->split_node(node);
-			return true;
-		}
-		return false;
-	}
+    int res;
+    if (this->iner_find(key,m_root, node, res)) {
+        return false;
+    } else {
+        assert(node != nullptr);
+        if (!isFull(node)) {
+            node->insertValue(key);
+            return true;
+        } else { //split
+            node->insertValue(key);
+            this->split_node(node);
+            return true;
+        }
+        return false;
+    }
 }
 
 void BTree::split_node(BTree::Node::Ptr node) {
-	auto C = this->make_node();
-	C->vals = node->vals;
-	
-	auto midle = C->vals[(C->vals.size() / 2)];
-	auto pos_half = C->vals.begin() + (C->vals.size() / 2 + 1);
-	C->vals.erase(C->vals.begin(), pos_half);
+    auto C = this->make_node();
+    C->vals = node->vals;
 
-	pos_half = node->vals.begin() + (node->vals.size() / 2);
-	node->vals.erase(pos_half, node->vals.end());
+    auto midle = C->vals[(C->vals.size() / 2)];
+    auto pos_half = C->vals.begin() + (C->vals.size() / 2 + 1);
+    C->vals.erase(C->vals.begin(), pos_half);
 
-	if (node->childs.size() > 0) {
-		std::vector<Node::Ptr> new_childs;
-		std::vector<Node::Ptr> old_childs;
-		for (auto c : node->childs) {
-			if (c->vals.front() > midle) {
-				new_childs.push_back(c);
-			} else {
-				old_childs.push_back(c);
-			}
-		}
-		node->childs = old_childs;
-		for (auto c : node->childs) {
-			c->parent = node;
-		}
-		C->childs = new_childs;
-		for (auto c : C->childs) {
-			c->parent = C;
-		}
-		
-	}
-	Node::Ptr node2insert = nullptr;
-	
-	if (auto parent = node->parent.lock()) {
-		node2insert = parent;
+    pos_half = node->vals.begin() + (node->vals.size() / 2);
+    node->vals.erase(pos_half, node->vals.end());
+
+    if (node->childs.size() > 0) {
+        std::vector<Node::Ptr> new_childs;
+        std::vector<Node::Ptr> old_childs;
+        for (auto c : node->childs) {
+            if (c->vals.front() > midle) {
+                new_childs.push_back(c);
+            } else {
+                old_childs.push_back(c);
+            }
+        }
+        node->childs = old_childs;
+        for (auto c : node->childs) {
+            c->parent = node;
+        }
+        C->childs = new_childs;
+        for (auto c : C->childs) {
+            c->parent = C;
+        }
+
+    }
+    Node::Ptr node2insert = nullptr;
+
+    if (auto parent = node->parent.lock()) {
+        node2insert = parent;
         node2insert->insertValue(midle);
 
         node2insert->insertChild(midle,C);
-		C->parent = node->parent;
-	} else {
-		node2insert = this->make_node();
-		m_root->parent = node2insert;
-		m_root = node2insert;
+        C->parent = node->parent;
+    } else {
+        node2insert = this->make_node();
+        m_root->parent = node2insert;
+        m_root = node2insert;
         node2insert->insertValue(midle);
         node2insert->childs.push_back(node);
         node2insert->childs.push_back(C);
-		node->parent = m_root;
-		C->parent = m_root;
-	}
+        node->parent = m_root;
+        C->parent = m_root;
+    }
 
-	
-	if (isFull(node2insert)) {
-		split_node(node2insert);
-	}
+
+    if (isFull(node2insert)) {
+        split_node(node2insert);
+    }
 }
 
 void BTree::print()const {
-	print(m_root);
+    print(m_root);
 }
 
 void BTree::print(const Node::Ptr& root)const {
-	std::cout << "{  [";
-	for (auto v : root->vals) {
-		std::cout << " " << v;
-	}
-	std::cout << "], ";
-	for (auto c : root->childs) {
-		print(c);
-	}
-	std::cout << "}"<<std::endl;
+    std::cout << "{  [";
+    for (auto v : root->vals) {
+        std::cout << " " << v;
+    }
+    std::cout << "], ";
+    for (auto c : root->childs) {
+        print(c);
+    }
+    std::cout << "}"<<std::endl;
 }
--- test/btree_test.cpp
+++ test/btree_test.cpp
@@ -9,53 +9,37 @@
 BOOST_AUTO_TEST_CASE(BtreeTest) {
 	const size_t n = 3;
 	trees::BTree t(n);
-	BOOST_CHECK_EQUAL(t.find(10), 0);
+    BOOST_CHECK_EQUAL(t.find(10), 0);
+
+    bool insert_status =false;
 
     for (int i = 0; i < int(2 * n); ++i) {
-		bool insert_status = t.insert(i);
+        insert_status = t.insert(i);
 		BOOST_CHECK(insert_status);
 		BOOST_CHECK_EQUAL(t.find(i), i);
 	}
-	/*t.print();*/
-	bool insert_status = t.insert(111);
+    insert_status = t.insert(111);
 	BOOST_CHECK(insert_status);
-    //t.print();
 	BOOST_CHECK_EQUAL(t.find(111), 111);
-	//t.print();
-	for (int i = 200; i < 300; i++) {
-//        t.print();
-        if (i == 203) {
-        //	t.print();
-        }
+
+    for (int i = 200; i < 300; i++) {
 		insert_status = t.insert(i);
-		//t.print();
 		if (!insert_status) {
 			std::stringstream ss;
 			ss << " insert " << i << " error";
 			BOOST_FAIL(ss.str());
 		}
-        if (i == 203) {
-    //		t.print();
-        }
-		BOOST_CHECK_EQUAL(t.find(i), i);
+        BOOST_CHECK_EQUAL(t.find(i), i);
 	}
 
 
 	for (int i = 50; i < 100; i++) {
-//        t.print();
-        if (i == 89) {
-            //t.print();
-        }
 		insert_status = t.insert(i);
-        //t.print();
 		if (!insert_status) {
 			std::stringstream ss;
 			ss << " insert " << i << " error";
 			BOOST_FAIL(ss.str());
 		}
-        if (i == 89) {
-            //t.print();
-        }
 		BOOST_CHECK_EQUAL(t.find(i), i);
 	}
 
